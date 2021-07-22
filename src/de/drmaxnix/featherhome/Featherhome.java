e4d4f260e14a5260d6ba0ff237d6c2956981e717
DrMaxNix <contact@drmaxnix.de>
2021-07-22 20:53:51 +0200

    ✨ Add real code

--- src/de/drmaxnix/featherhome/Featherhome.java
+++ src/de/drmaxnix/featherhome/Featherhome.java
@@ -2,13 +2,32 @@ package de.drmaxnix.featherhome;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Featherhome extends JavaPlugin {
+	private static Featherhome instance;
+	
+	public Featherhome(){
+		if(Featherhome.instance == null){
+			Featherhome.instance = this;
+		}
+	}
+	
+	public static Featherhome getInstance(){
+		return Featherhome.instance;
+	}
+	
 	@Override
 	public void onEnable(){
-		System.out.println("enabled");
+		//bind registered commands
+		this.getCommand("home").setExecutor(new Cmd_home());
+		this.getCommand("sethome").setExecutor(new Cmd_sethome());
+		
+		//print debug
+		//System.out.println("[FeatherHome] by DrMaxNix | www.drmaxnix.de");
+		System.out.println("[FeatherHome] Enabled FeatherHome");
 	}
 	
 	@Override
 	public void onDisable(){
-		System.out.println("disbaled");
+		//print debug
+		System.out.println("[FeatherHome] Disabled FeatherHome");
 	}
 }
